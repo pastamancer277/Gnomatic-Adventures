3874f17787fcb7ddabec50e8b2e6a9d260cc2a5c
alyssa576 <alyssawright19@icloud.com>
2026-04-28 12:13:12 -0400

    cut scene

--- scripts/screens/cut_scene.gd
+++ scripts/screens/cut_scene.gd
@@ -11,4 +11,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("MeleeAttack"):
-		queue_free()
+		pass
