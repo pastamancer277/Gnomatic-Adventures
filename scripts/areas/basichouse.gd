84b86c44cef6d83a2ef4da470b9ede8003ad9840
pastamancer277 <picklescott828@gmail.com>
2026-05-06 12:37:26 -0400

    Basic house transports

---
+++ scripts/areas/basichouse.gd
@@ -0,0 +1,21 @@
+extends area
+
+var exitLoc
+var exitPos
+
+func enterFrom(node:String):
+	exitLoc=node
+	exitPos = player.global_position
+	player.global_position = $EnterPoints/Door.global_position
+
+func _on_exit_body_entered(body: Node2D) -> void:
+	if(body.name == "Player"):
+		Global.scene_transit=true
+		locationChange=exitLoc
+		player.global_position = exitPos
+		player.global_position.y = player.global_position.y + 15
+
+
+func _on_exit_body_exited(body: Node2D) -> void:
+	if(body.name == "Player"):
+		Global.scene_transit = false
