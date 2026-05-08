572dc6e81041d64d3ffbdca08c5d96ba2083a2a9
maddie-MandM <maddiemb12@icloud.com>
2026-05-08 14:18:56 -0400

    map + art updates

--- scripts/NPCs/City2dialoge.gd
+++ scripts/NPCs/City2dialoge.gd
@@ -4,15 +4,15 @@ extends dialogue
 
 func _ready() -> void:
 	dialogueBox=$/root/Main/UI/Dialogue
-	person_name = "George"
+	person_name = "Stella"
 	tex = PortableCompressedTexture2D.new()
-	var im = load("res://assets/sprites/characters/Gnomester.png").get_image()
+	var im = load("res://assets/sprites/characters/femalegnome1.png").get_image()
 	im = im.get_region(Rect2(im.get_width()/3,0, im.get_width()/2, im.get_height()/1.5))
 	tex.create_from_image(im, PortableCompressedTexture2D.COMPRESSION_MODE_LOSSLESS)
 
 func interact():
 	if(run==1):
-		dialogueBox.dialogue("I've heard there is a farmer who sells pomegranates. He lives past the village.", self,person_name, tex)
+		dialogueBox.dialogue("Hey don't go down there. I've heard the Wizard Cat lives there.", self,person_name, tex)
 		dialogueBox.setDialogueOption("Thank you!", 0)
 
 func playerResponse(key: int):
