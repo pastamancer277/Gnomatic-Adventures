75a40b6b682946ab07f0ba53a1be5a21648034e7
pastamancer277 <picklescott828@gmail.com>
2026-05-10 19:42:40 -0400

    Bug fixes

--- scripts/NPCs/forest3Dialogue.gd
+++ scripts/NPCs/forest3Dialogue.gd
@@ -70,3 +70,12 @@ func playerResponse(key: int):
 		interact()
 	else: if(run==5):
 		pass
+
+func setQuest(name):
+			quest_item = quest_item_scene.instantiate()
+			item_slot = item_slot_scene.instantiate()
+			item_slot.set_item(wood_item, 1)
+			var items = [item_slot]
+			quest_item.new_quest(items, "House For-Rest", "Bring Jack wood for a house")
+			quest_menu = $"/root/Main/UI/QuestMenu"
+			quest_menu.get_child(0).add_child(quest_item)
